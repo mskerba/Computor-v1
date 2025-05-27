c39b935b8ff9b7ac07e019c984b14757e8e70feb
mskerba <monaimskerba@gmail.com>
2025-05-27 18:28:16 +0100

    all is done

--- PolynomialSolver.hpp
+++ PolynomialSolver.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <regex>
 #include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
-#include <iostream>
+
 #include "PolynomialSolver.hpp"
+
+int valid(std::string equation) {
+    std::regex pattern(R"(^([-+]?\d+(\.\d+)?\s\*\sX\^\d+(\s[-+]\s\d+(\.\d+)?\s\*\sX\^\d+)*)\s=\s([-+]?\d+(\.\d+)?\s\*\sX\^\d+(\s[-+]\s\d+(\.\d+)?\s\*\sX\^\d+)*))");
+
+    bool ret = std::regex_match(equation, pattern);
+    return ret;
+}
 
 int main(int ac , char **av)
 {
     if (ac == 1) {
         std::cout << "Adding the argument!"<< std::endl;
+        return 0;
+    } else if (!valid(av[1])) {
+        std::cout << "Invalid equation" << std::endl;
         return 0;
     }
     // "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0";
